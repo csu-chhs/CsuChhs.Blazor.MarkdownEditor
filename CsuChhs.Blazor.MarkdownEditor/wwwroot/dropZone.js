b994e39a3651a1f467d24f3c301ae4ddaeae5d96
Derek Stegelman <dstegelman@gmail.com>
2025-02-17 06:50:29 -0700

    Initial pasting abilities

---
+++ CsuChhs.Blazor.MarkdownEditor/wwwroot/dropZone.js
@@ -0,0 +1,15 @@
+﻿export function initializeDropZone(dropZoneElement, inputFile) {
+    function onPaste(e) {
+        inputFile.files = e.clipboardData.files;
+        const event = new Event('change', { bubbles: true });
+        inputFile.dispatchEvent(event);
+    }
+    
+    dropZoneElement.addEventListener('paste', onPaste);
+    
+    return {
+        dispose: () => {
+            dropZoneElement.removeEventListener('paste', onPaste);
+        }
+    }
+}
